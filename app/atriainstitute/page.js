9bb3c1933eb2cdb2ce0b7ba851e5e9912b672548
Jecy Jehu <jecyjehu@outlook.com>
2023-05-16 14:56:31 +0100

    deploy

--- app/atriainstitute/page.js
+++ app/atriainstitute/page.js
@@ -1,11 +1,17 @@
+import Livingroom from '@components/institute/livingroom';
 import Hero from '../../components/institute/hero';
 import Scientists from '../../components/institute/scientists';
+import Science from '@components/institute/science';
+import Medicine from '@components/institute/medicine';
 
 const Atriainstitute = () => {
   return (
     <section>
       <Hero />
       <Scientists />
+      <Livingroom />
+      <Science />
+      <Medicine />
     </section>
   )
 }
---
+++ components/institute/livingroom.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+
+const Livingroom = () => {
+  return (
+    <section className='relative bg-contain w-full'>
+        <Image src='/assets/images/livingroom2.jpg' alt='/' width={1400} height={1500} />
+      </section>
+  )
+}
+
+export default Livingroom;
