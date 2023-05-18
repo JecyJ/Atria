71d915445c7c7d5347a89ae724f1286757eb084b
Jecy Jehu <jecyjehu@outlook.com>
2023-05-18 13:27:25 +0100

    deploy

--- app/atriahealth/page.js
+++ app/atriahealth/page.js
@@ -1,8 +1,33 @@
+import Footer from "@components/atria/footer";
+import Subscribe from "@components/atria/subscribe";
+import Advanced from "@components/health/advanced";
+import Balls from "@components/health/balls";
+import Bigboulder from "@components/health/bigboulder";
+import Cofounder from "@components/health/cofounder";
+import Hero from "@components/health/hero";
+import Knowledge from "@components/health/knowledge";
+import Learnmore from "@components/health/learnmore";
+import Painting from "@components/health/painting";
+import Stetoscope from "@components/health/stetoscope";
+import Contact from "@components/institute/contact";
 
 
 const Atriahealth = () => {
   return (
-    <div>Atriahealth</div>
+    <section>
+      <Hero />
+      <Cofounder />
+      <Bigboulder />
+      <Advanced />
+      <Knowledge />
+      <Balls />
+      <Stetoscope />
+      <Painting />
+      <Learnmore />
+      <Contact />
+      <Subscribe />
+      <Footer />
+    </section>
   )
 }
 
