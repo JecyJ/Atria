43c4f78d37704f6eeed54a13973375f005ad9bd5
Jecy Jehu <jecyjehu@outlook.com>
2023-05-18 14:33:20 +0100

    deploy

--- components/academy/hero.js
+++ components/academy/hero.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className='grid content-center pt-96 w-full h-screen'>
+    <section className='grid content-center pt-48 w-full h-screen'>
       <div className='z-[-2]'>
         <Image src='/assets/images/stetoscope.png' alt='/' layout='fill' objectFit='cover' />
       </div>
-      <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-20 text-white'>
+      <div className='max-w-[520px] sm:max-w-[620px] md:max-w-[1250px] w-full m-auto space-y-6 text-white'>
         <h1 className='text-lg md:text-3xl'>Guiding a transformation in medicine</h1>
         <div className='text-6xl md:text-9xl'>
-            <h1>Discovering <br className="md:hidden"/>what is</h1>
+            <h1>Discovering <br className="md:hidden pt"/>what is</h1>
             <h1>biologically possible.</h1>
         </div>
       </div>
